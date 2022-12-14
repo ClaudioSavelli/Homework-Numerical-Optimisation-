870ada4a18f929e53f2943cc164ee0caed86dad6
Bruno Spaccavento <s314908@studenti.polito.it>
2022-12-15 00:47:35 +0100

    Aggiornamento 1.2
    - Aggiunta funzioni extnd_rosenb e grad_extnd_rosenb
    - Modifica di [i, 0] in [i] per ogni valore di i

--- Homework Numerical Optimisation/Unconstrained Optimisation/functions.py
+++ Homework Numerical Optimisation/Unconstrained Optimisation/functions.py
@@ -5,7 +5,7 @@ h = np.sqrt(np.finfo(float).eps)
 
 def rosenbrock(x: np.ndarray) -> float:
     #Evaluation of the Rosenbrock function in the point x
-    return 100*(x[1, 0]-x[0, 0]**2)**2 + (1-x[0, 0])**2
+    return 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
 
 
 def grad_rosenbrock(x: np.ndarray, fin_diff: bool, type: str) -> np.ndarray:
@@ -24,18 +24,18 @@ def grad_rosenbrock(x: np.ndarray, fin_diff: bool, type: str) -> np.ndarray:
     grad = np.empty((num,1))
     if fin_diff == True:
         e = np.identity(num)
-        if (type == "forward" or type == "backward"): 
+        if (type == "fw" or type == "bw"): 
             fx = rosenbrock(x)
         for i in range(0, num):
-            if(type == "forward"): 
-                grad[i, 0] = (rosenbrock(x+h*e[:, i].reshape(-1, 1)) - fx) / h
-            elif(type == "backward"): 
-                grad[i, 0] = -(rosenbrock(x-h*e[:, i].reshape(-1, 1)) - fx) / h
+            if(type == "fw"): 
+                grad[i] = (rosenbrock(x+h*e[:, i].reshape(-1, 1)) - fx) / h
+            elif(type == "bw"): 
+                grad[i] = -(rosenbrock(x-h*e[:, i].reshape(-1, 1)) - fx) / h
             else:
-                grad[i, 0] = (rosenbrock(x+h*e[:, i].reshape(-1, 1)) - rosenbrock(x-h*e[:, i].reshape(-1, 1))) / (2*h)
+                grad[i] = (rosenbrock(x+h*e[:, i].reshape(-1, 1)) - rosenbrock(x-h*e[:, i].reshape(-1, 1))) / (2*h)
     else:
-        grad[0, 0] = 400*x[0, 0]**3 - 400*x[0, 0]*x[1, 0] + 2*x[0, 0] - 2
-        grad[1, 0] = 200*(x[1, 0] - x[0, 0]**2)
+        grad[0] = 400*x[0]**3 - 400*x[0]*x[1] + 2*x[0] - 2
+        grad[1] = 200*(x[1] - x[0]**2)
     return grad
 
 
@@ -48,20 +48,20 @@ def extnd_powell(x: np.ndarray) -> float:
         match k % 4:
             case 1:
                 k -= 1
-                return x[k, 0] + 10*x[k+1, 0]
+                return x[k] + 10*x[k+1]
             case 2:
                 k -= 1
-                return sqrt(5)*(x[k+1, 0]-x[k+2, 0])
+                return sqrt(5)*(x[k+1]-x[k+2])
             case 3:
                 k -= 1
-                return (x[k-1, 0] - 2*x[k, 0])**2
+                return (x[k-1] - 2*x[k])**2
             case 0:
                 k -= 1
-                return sqrt(10)*(x[k-3, 0] - x[k, 0])**2
+                return sqrt(10)*(x[k-3] - x[k])**2
     
     z = np.empty((num, 1))
     for k in range(0, num):
-        z[k, 0] = f(x, k+1)
+        z[k] = f(x, k+1)
     return (0.5 * np.sum(z**2, axis=0))[0]
 
 
@@ -71,33 +71,87 @@ def grad_extnd_powell(x: np.ndarray, fin_diff: bool, type: str) -> np.ndarray:
         raise Exception("Array length must be multiple of 4.")
     
     def df(x: np.ndarray, k: int) -> float:
+        xk = x[k-1]
         match k % 4:
             case 1:
                 k -= 1
-                return x[k, 0] + 10*x[k+1, 0] + 20*(x[k, 0] - x[k+3, 0])**3
+                return xk + 10*x[k+1] + 20*(xk - x[k+3])**3
             case 2:
                 k -= 1
-                return 10*(x[k-1, 0]+10*x[k, 0]) + 2*(x[k, 0]-2*x[k+1, 0])**3
+                return 10*(x[k-1]+10*xk) + 2*(xk-2*x[k+1])**3
             case 3:
                 k -= 1
-                return 5*(x[k, 0] - x[k+1, 0]) + 4*(2*x[k, 0] - x[k-1, 0])**3
+                return 5*(xk - x[k+1]) + 4*(2*xk - x[k-1])**3
             case 0:
                 k -= 1
-                return 5*(x[k, 0] - x[k-1, 0]) + 20*(x[k, 0] - x[k-3, 0])**3
+                return 5*(xk - x[k-1]) + 20*(xk - x[k-3])**3
 
     grad = np.empty((num, 1))
     if fin_diff == True:
         e = np.identity(num)
-        if (type == "forward" or type == "backward"): 
+        if (type == "fw" or type == "bw"): 
             fx = extnd_powell(x)
         for i in range(0, num):
-            if(type == "forward"): 
-                grad[i, 0] = (extnd_powell(x+h*e[:, i].reshape(-1, 1)) - fx) / h
-            elif(type == "backward"): 
-                grad[i, 0] = -(extnd_powell(x-h*e[:, i].reshape(-1, 1)) - fx) / h
+            if(type == "fw"): 
+                grad[i] = (extnd_powell(x+h*e[:, i].reshape(-1, 1)) - fx) / h
+            elif(type == "bw"): 
+                grad[i] = -(extnd_powell(x-h*e[:, i].reshape(-1, 1)) - fx) / h
             else:
-                grad[i, 0] = (extnd_powell(x+h*e[:, i].reshape(-1, 1)) - extnd_powell(x-h*e[:, i].reshape(-1, 1))) / (2*h)
+                grad[i] = (extnd_powell(x+h*e[:, i].reshape(-1, 1)) - extnd_powell(x-h*e[:, i].reshape(-1, 1))) / (2*h)
     else:
         for k in range(0, num):
-            grad[k, 0] = df(x, k+1)
+            grad[k] = df(x, k+1)
+    return grad
+
+
+def extnd_rosenb(x: np.ndarray) -> float:
+    num = x.shape[0]
+    if num % 2 != 0:
+        raise Exception("Array length must be multiple of 2.")
+    
+    def f(x: np.ndarray, k: int) -> float:
+        match k % 2:
+            case 1:
+                k -= 1
+                return 10*(x[k]**2 - x[k+1])
+            case 0:
+                k -= 1
+                return x[k-1] - 1
+    
+    z = np.empty((num, 1))
+    for k in range(0, num):
+        z[k] = f(x, k+1)
+    return (0.5 * np.sum(z**2, axis=0))[0]
+
+
+def grad_extnd_rosenb(x: np.ndarray, fin_diff: bool, type: str) -> np.ndarray:
+    num = x.shape[0]
+    if num % 2 != 0:
+        raise Exception("Array length must be multiple of 2.")
+    
+    def df(x: np.ndarray, k: int) -> float:
+        xk = x[k-1]
+        match k % 2:
+            case 1:
+                k -= 1
+                return 200*xk**3 - 200*xk*x[k+1] + xk - 1
+            case 0:
+                k -= 1
+                return 100*(xk - x[k-1]**2)
+
+    grad = np.empty((num, 1))
+    if fin_diff == True:
+        e = np.identity(num)
+        if (type == "fw" or type == "bw"): 
+            fx = extnd_rosenb(x)
+        for i in range(0, num):
+            if(type == "fw"): 
+                grad[i] = (extnd_rosenb(x+h*e[:, i].reshape(-1, 1)) - fx) / h
+            elif(type == "bw"): 
+                grad[i] = -(extnd_rosenb(x-h*e[:, i].reshape(-1, 1)) - fx) / h
+            else:
+                grad[i] = (extnd_rosenb(x+h*e[:, i].reshape(-1, 1)) - extnd_rosenb(x-h*e[:, i].reshape(-1, 1))) / (2*h)
+    else:
+        for k in range(0, num):
+            grad[k] = df(x, k+1)
     return grad
