06758f0e0c793b827269501b1d8c41c94a909c3d
Bruno Spaccavento <s314908@studenti.polito.it>
2022-12-31 18:41:40 +0100

    Aggiornamento 5.0
    - Scrittura function, main, pcg

---
+++ Homework Numerical Optimisation/Constrained Optimisation/proj_gradient_method.py
@@ -0,0 +1,89 @@
+import numpy as np
+from function import *
+
+def project(x: np.ndarray, X: dict) -> np.ndarray:
+    for v in X.values():
+        low = v[0][0]
+        upp = v[0][1]
+        start = v[1][0]
+        stop = v[1][1]
+        x[start:stop] = np.where(x[start:stop] > upp, upp, x[start:stop])
+        x[start:stop] = np.where(x[start:stop] < low, low, x[start:stop])
+    return x
+
+
+def projected_gradient_bcktrck(x0: np.ndarray, box: dict, gamma: float, kmax: int, tolgrad: float, tolx: float, c1: float, rho: float, btmax: int, fin_diff: bool, fd_type: str, kgrad: int):
+    
+    ''' Function that performs the steepest descent optimization method for a given function.
+    
+    INPUTS:
+    x0 = n−dimensional column vector;
+    f = string that represent the function I want to use between the one stored there;
+    alpha0 = the initial factor that multiplies the descent direction at each iteration;
+    kmax = maximum number of iterations permitted;
+    tolgrad = value used as stopping criterion w.r.t. the norm of the gradient;
+    c1 = factor of the Armijo condition that must be a scalar in (0,1);
+    rho = fixed factor, lesser than 1, used for reducing alpha0;
+    btmax = maximum number of steps for updating alpha during the backtracking strategy.
+    fin_diff = choose between using the finite differences method for the evaluation of the gradient or not
+    fd_type = if fin_diff == True, choose between centered/forward/backword finite differences method
+    
+    OUTPUTS:
+    xk = the last x computed by the function;
+    fk = the value f(xk);
+    gradfk_norm = value of the norm of gradf(xk)
+    k = index of the last iteration performed
+    x_seq = n−by−k matrix where the columns are the xk computed during the iterations
+    bt_seq = k vector whose elements are the number of backtracking '''
+    
+    x_seq = x0.reshape(1, -1)
+    bt_seq = np.empty((1, 1))
+    f_seq = np.empty((1, 1))
+    gradf_norm_seq = np.empty((1, 1))
+    deltax_norm_seq = np.empty((1, 1))
+    xk = x0
+    fk = 0
+    k = 0
+    gradfk_norm = 0
+    deltaxk_norm = 1
+    alpha0 = 1
+    alphak = alpha0
+    
+    fk = rosenbrock(xk)
+    f_seq[0] = fk
+    gradfk_norm = np.linalg.norm(grad_rosenbrock(xk, fin_diff, fd_type, kgrad), 2)
+    gradf_norm_seq[0] = gradfk_norm
+    deltax_norm_seq[0] = deltaxk_norm
+    
+    while k < kmax and gradfk_norm > tolgrad and deltaxk_norm > tolx:
+        gradfk = grad_rosenbrock(xk, fin_diff, fd_type, kgrad)
+        pk = -gradfk
+        xhat = project(xk + gamma*pk, box)
+        pik = xhat - xk
+        xnew = xk + alphak*pik
+        fnew = rosenbrock(xnew)
+        
+        bt = 0
+        while (bt < btmax) and (fnew > fk + c1*alphak*(gradfk @ pik)):
+            # update alpha
+            alphak = rho*alphak
+            xnew = xk + alphak*pik
+            fnew = rosenbrock(xnew)
+            bt = bt + 1
+        alphak = alpha0
+        
+        deltaxk_norm = np.linalg.norm(xnew - xk, 2)
+        deltax_norm_seq = np.append(deltax_norm_seq, np.array([[deltaxk_norm]]))
+        xk = xnew
+        fk = fnew
+        gradfk_norm = np.linalg.norm(grad_rosenbrock(xk, fin_diff, fd_type, kgrad), 2)
+        x_seq = np.append(x_seq, xk.reshape(1, -1), axis=0)
+        f_seq = np.append(f_seq, np.array([[fk]]))
+        gradf_norm_seq = np.append(gradf_norm_seq, np.array([[gradfk_norm]]))
+        if k == 0:
+            bt_seq[0] = bt
+        else:
+            bt_seq = np.append(bt_seq, np.array([[bt]]))
+        k = k + 1
+        
+    return x_seq, f_seq, gradf_norm_seq, deltax_norm_seq, k, bt_seq
